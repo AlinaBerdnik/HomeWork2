0ddfe8db39ebf8ca480ba643cf89edc89a0391ac
Alina <alina-berdnikova@inbox.ru>
2022-12-24 22:09:51 +0300

    сделала задание

---
+++ zadaniye15/Program.cs
@@ -0,0 +1,15 @@
+﻿{int input = int.Parse(Console.ReadLine());
+
+            if (input > 0 & input < 6)
+            {
+                Console.WriteLine("Будний день");
+            }
+            else if (input >= 6 & input <= 7)
+            {
+                Console.WriteLine("Выходной");
+            }
+            else
+            {
+                Console.WriteLine("Нет такого дня");
+            }
+}          
